8955fc2d956b089f88ad347abab599b685b87a02
Adrian Molofsky <molofsky@stanford.edu>
2023-10-31 15:19:45 -0700

    Create x-y-z-counter.v

---
+++ x-y-z-counter.v
@@ -0,0 +1,21 @@
+/* 
+*
+* Verification Example:
+* The value of y is always supposed to be x + 1
+* The value of z is always supposed to be y + 1
+*
+* @author Adrian Molofsky
+* @version 10/31/23
+*
+*/
+
+always @(posedge clk) begin
+  if (reset) begin
+      x <= 0; y <= 1; z <= 2;
+  end
+  else if (a == 0) begin
+      z <= y + 2;
+      y <= z:
+      x <= y;
+  end
+end
