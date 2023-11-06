31e4d53446dee7a36c23315fde9fc8f8c26bce68
Adrian Molofsky <molofsky@stanford.edu>
2023-11-06 09:14:15 -0800

    Create x1-x2-counter.v

---
+++ x1-x2-counter.v
@@ -0,0 +1,15 @@
+/*
+* Create an inductive case for x1 <= 6
+* Fails for x1 = 6 which is not reachable from initial state x1 = 0
+*
+* @author Adrian Molofsky
+* @version 11/06/23
+*/
+
+logic [3:0] x1 = 0;
+logic [3:0] x2 = 0;
+always @(posedge clk)
+begin
+  x2 <= x1 + 1;
+  x1 <= (x1 + 1) % 5;
+end
