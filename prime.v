a27e0b7c43e3a279d80496cb0f17af2d7d0ca4df
Adrian Molofsky <molofsky@stanford.edu>
2025-05-31 11:48:34 -0700

    Create prime.v

---
+++ prime.v
@@ -0,0 +1,16 @@
+/* Verilog function to check if a four bit input is prime
+*
+* @author Adrian Molofsky
+* @version 1/6/23
+*/
+module prime(in, isprime); 
+	input [3:0] in;
+	output reg isprime;
+	
+	always @* begin
+		case(in)
+		1, 2 , 3, 5, 7, 11, 13: isprime = 1’b1;
+		default: isprime = 1’b0;
+		endcase
+	end
+endmodule
